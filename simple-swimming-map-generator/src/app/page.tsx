f355e4f60ae6ccd888156b1920a41985cca72da3
keldensky <kelden.dorji@sky.uk>
2023-05-20 01:22:55 +0200

    Added Dummy start point

---
+++ simple-swimming-map-generator/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="Nav-Bar flex gap: 25%">
+        <span>a</span>
+        <span className="">b</span>
+        <span>c</span>
+        <span>d</span>
+      </div>
+    </div>
+  );
+}
